598ae6e2ebe7f6509017d63252e8900ddfb945d5
CLarmour <connorlarmour@outlook.com>
2015-11-17 16:48:38 +0000

    Completed exercises 6-8

--- app/actions.rb
+++ app/actions.rb
@@ -11,3 +11,21 @@ end
 get '/messages/new' do
   erb :'messages/new'
 end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author: params[:author]
+  )
+  if @message.save
+  redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
